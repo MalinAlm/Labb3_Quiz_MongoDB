08867181d1fd0a184c5cf0970fa6acc45bf60f34
sweviceroy <113086422+sweviceroy@users.noreply.github.com>
2026-01-21 12:23:15 +0100

    Chore: add mongodb Settings and context skeleton only, missing documents, continue later

---
+++ Labb3_Quiz/Data/Mongo/MongoSettings.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using Labb3_Quiz_MongoDB.Data.Mongo.Documents;
+
+namespace Labb3_Quiz_MongoDB.Data.Mongo
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(MongoSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            _database = client.GetDatabase(settings.DatabaseName);
+        }
+
+        public IMongoCollection<QuestionPackDocument> QuestionPacks =>
+            _database.GetCollection<QuestionPackDocument>("QuestionPacks");
+
+        public IMongoCollection<CategoryDocument> Categories =>
+            _database.GetCollection<CategoryDocument>("Categories");
+    }
+}
